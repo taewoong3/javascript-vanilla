98ffd6466fc2ccb2059af6680f94a5a231199c26
taewoong3 <83555445+taewoong3@users.noreply.github.com>
2021-12-22 21:07:51 +0900

    update

--- js/background.js
+++ js/background.js
@@ -1,13 +1,4 @@
-const images = [
-  "1.jpg",
-  "2.jpg",
-  "3.jpg",
-  "4.jpg",
-  "5.jpg",
-  "6.jpg",
-  "7.jpg",
-  "8.jpg",
-];
+const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"];
 
 const todayImage = images[Math.floor(Math.random() * images.length)];
 
